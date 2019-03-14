0ae8cdb5d173595797dfe1344b55755af45e75fb
rkozicki <rkozicki@enigma.com.pl>
2019-03-14 12:42:48 +0100

    Add example code to trigger bug

--- Example/JTAppleCalendar iOS Example/Example Calendars/TestRangeSelectionViewController.swift
+++ Example/JTAppleCalendar iOS Example/Example Calendars/TestRangeSelectionViewController.swift
@@ -13,6 +13,8 @@ class TestRangeSelectionViewController: UIViewController {
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var calendarView: JTAppleCalendarView!
     let df = DateFormatter()
+    var startDate: Date?
+    var endDate: Date?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +25,6 @@ class TestRangeSelectionViewController: UIViewController {
         
         calendarView.isRangeSelectionUsed = true
         calendarView.allowsMultipleSelection = true
-        
     }
     
     func setupMonthLabel(date: Date) {
@@ -71,10 +72,50 @@ class TestRangeSelectionViewController: UIViewController {
 }
 
 extension TestRangeSelectionViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
-    
-    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        handleConfiguration(cell: cell, cellState: cellState)
+    func calendar(_: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        print("didSelectDate, cell = \(cell)")
+        guard let validCell = cell as? TestRangeSelectionViewControllerCell else { return }
+        if startDate == nil {
+            let selectedDates = calendarView.selectedDates
+            calendarView.deselect(dates: selectedDates, triggerSelectionDelegate: false)
+            startDate = date
+        }
+        else if endDate == nil, let start = startDate {
+            if date < start {
+                startDate = date
+                endDate = start
+            } else {
+                endDate = date
+            }
+        }
+
+        if let start = startDate, let end = endDate {
+            calendarView.selectDates(from: start, to: end,  triggerSelectionDelegate: false, keepSelectionIfMultiSelectionAllowed: true)
+            startDate = nil
+            endDate = nil
+        }
+        handleConfiguration(cell: validCell, cellState: cellState)
     }
+
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        guard let validCell = cell as? TestRangeSelectionViewControllerCell else { return }
+        handleConfiguration(cell: validCell, cellState: cellState)
+    }
+
+    func calendar(_: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        print("didDeselectDate")
+        if startDate == nil, endDate == nil {
+            let selectedDates = calendarView.selectedDates
+            calendarView.deselect(dates: selectedDates, triggerSelectionDelegate: false)
+            calendarView.selectDates([date])
+        }
+        else {
+            guard let validCell = cell as? TestRangeSelectionViewControllerCell else { return }
+            startDate = nil
+            handleConfiguration(cell: validCell, cellState: cellState)
+        }
+    }
+
     
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "cell", for: indexPath) as! TestRangeSelectionViewControllerCell
@@ -82,25 +123,18 @@ extension TestRangeSelectionViewController: JTAppleCalendarViewDelegate, JTApple
         self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
         return cell
     }
-    
+
     func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
         setupMonthLabel(date: visibleDates.monthDates.first!.date)
     }
-    
-    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        handleConfiguration(cell: cell, cellState: cellState)
-    }
-    
-    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        handleConfiguration(cell: cell, cellState: cellState)
-    }
-    
+
+
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         let df = DateFormatter()
         df.dateFormat = "yyyy MM dd"
-        
-        let startDate = df.date(from: "2017 01 01")!
-        let endDate = df.date(from: "2017 12 31")!
+
+        let startDate = df.date(from: "2018 01 01")!
+        let endDate = df.date(from: "2018 12 31")!
         
         let parameter = ConfigurationParameters(startDate: startDate,
                                                 endDate: endDate,
